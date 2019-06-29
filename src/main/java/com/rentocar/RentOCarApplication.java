5b5fe589429cedd7dd2d47a6e4f96e217b529744
Akash Chandra <akasch23@in.ibm.com>
2019-06-29 16:13:29 +0530

    Initial Commit

---
+++ src/main/java/com/rentocar/RentOCarApplication.java
@@ -0,0 +1,13 @@
+package com.rentocar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentOCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentOCarApplication.class, args);
+	}
+
+}
